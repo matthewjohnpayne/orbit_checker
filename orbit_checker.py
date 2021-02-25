36b73ba222a5f74e0b5ee46f4d2221153f2b215d
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-02-25 14:37:08 -0500

    init code commit

--- orbit_checker.py
+++ orbit_checker.py
@@ -109,7 +109,7 @@ def check_single_designation( unpacked_provisional_designation , dbConnIDs, dbCo
     
     # Understand the quality of any orbfit-orbit currently in the database ...
     # - Not clear where we want to be doing this, but while developing I am doing this here ...
-    quality_dict = dbConnOrbs.get_quality_json(unpacked_provisional_designation)
+    quality_dict = dbConnOrbs.get_quality_json(unpacked_provisional_designation)[0]['quality_json']
     print('quality_dict', quality_dict)
     status_dict  = assess_quality_dict(quality_dict)
     print('status_dict',status_dict)
