9c7953cd9c3b3c09d4242db9cbe204df6c06932a
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-02-26 09:47:46 -0500

    developing logic

--- orbit_checker.py
+++ orbit_checker.py
@@ -50,6 +50,34 @@ status_dict = {
     29  : "Orbit Exists: As yet unclassified",
 }
 
+# Alternative way of define possible orbit/designation "status"
+boolean_dict = {
+
+# Overall designation status
+'IS_PRIMARY_UNPACKED_DESIGNATION' : False,
+
+# Whether an orbit exists anywhere
+'IS_IN_ORBFIT_RESULTS'            : False,
+'IS_IN_COMET_RESULTS'             : False,
+'IS_IN_SATELLITE_RESULTS'         : False,
+'IS_IN_NO_RESULTS'                : False,
+
+# If has orbfit results, what is overall summary of the "quality-json"
+'HAS_BAD_QUALITY_DICT'            : False,
+'HAS_INTERMEDIATE_QUALITY_DICT'   : False,
+'HAS_GOOD_QUALITY_DICT'           : False,
+
+# Are there any problems when we try to get observations to do fit
+'HAS_NO_OBSERVATIONS'             : False,
+'HAS_FEW_OBSERVATIONS'            : False,
+'HAS_MANY_OBSERVATIONS'           : False,
+
+# Are there any obvious outlier tracklets when we run fit (bad tracklet dict)
+'HAS_ERRONEOUS_TRACKLETS'         : False,
+
+}
+
+
 def check_multiple_designations( method = None , size=0 ):
     """
     Outer loop-function to allow us to check a long list of designations
@@ -57,13 +85,15 @@ def check_multiple_designations( method = None , size=0 ):
     
     # Setting up connection objects...
     # (i)to PP's ID-Query routines ...
-    dbConnIDs  = query_ids.QueryCurrentID()
     # (ii) to MJP's Orb-Query routines ...
+    dbConnIDs  = query_ids.QueryCurrentID()
     dbConnOrbs = query_orbs.QueryOrbfitResults()
+
 
     # Setting up a default array for development ...
     primary_designations_array = np.array( ['2016 EN210'])#, '2009 DU157', '2015 XB53', '2015 XX229', '2011 BU37'] )
-        
+
+
     # Get a list of primary designations from the current_identifications table in the database
     if method in ['ALL' ,'RANDOM']:
         pass
@@ -83,6 +113,7 @@ def check_multiple_designations( method = None , size=0 ):
     
     
     
+    
 def check_single_designation( unpacked_provisional_designation , dbConnIDs, dbConnOrbs, FIX=False):
     '''
     Do a buunch of checks on a single designation
@@ -94,25 +125,27 @@ def check_single_designation( unpacked_provisional_designation , dbConnIDs, dbCo
     # Is this actually a primary unpacked_provisional_designation ?
     # - If being called from a list pulled from the identifications tables, then this step is unnecessary
     # - But I provide it for safety
-    isValidPrimary = dbConnIDs.is_valid_unpacked_primary_desig(unpacked_provisional_designation)
-    assert isValidPrimary
+    boolean_dict['IS_PRIMARY_UNPACKED_DESIGNATION'] = dbConnIDs.is_valid_unpacked_primary_desig(unpacked_provisional_designation)
 
     # Update the primary_objects table to flag whether we have an orbit in the orbfit-results table
     # NB(1) : If there is *no* match, then the returned value for orbfit_results_id == False
     # NB(2) : This is not logically great, should really be checking not in comets, etc,
     #         but for now, while developing, this is not something to worry about while the comet tble is empty
-    orbfit_results_boolean       = dbConnOrbs.has_orbfit_result(unpacked_provisional_designation)
+    boolean_dict['IS_IN_ORBFIT_RESULTS']        = dbConnOrbs.has_orbfit_result(unpacked_provisional_designation)
+    boolean_dict['IS_IN_COMET_RESULTS']         = False
+    boolean_dict['IS_IN_SATELLITE_RESULTS']     = False
+    boolean_dict['IS_IN_NO_RESULTS']            = not boolean_dict['IS_IN_ORBFIT_RESULTS']
     print('orbfit_results_boolean=', orbfit_results_boolean)
-    if orbfit_results_boolean:
-        dbConnOrbs.set_orbfit_results_flags_in_primary_objects( unpacked_provisional_designation ,
-                                                                orbfit_results_boolean   )
+    #if orbfit_results_boolean:
+    #    dbConnOrbs.set_orbfit_results_flags_in_primary_objects( unpacked_provisional_designation ,
+    #                                                                boolean_dict['IS_IN_ORBFIT_RESULTS']   )
     
     # Understand the quality of any orbfit-orbit currently in the database ...
     # - Not clear where we want to be doing this, but while developing I am doing this here ...
     quality_dict = dbConnOrbs.get_quality_json(unpacked_provisional_designation)[0]['quality_json']
+    assess_quality_dict(quality_dict , boolean_dict)
     print('quality_dict', quality_dict)
-    status_dict  = assess_quality_dict(quality_dict)
-    print('status_dict',status_dict)
+    print('boolean_dict',status_dict)
     
     # Attempt to fit the orbit using the "orbit_pipeline_wrapper"
     #result_dict = call_orbfit_via_commandline_update_wrapper(unpacked_provisional_designation)
@@ -128,6 +161,8 @@ def check_single_designation( unpacked_provisional_designation , dbConnIDs, dbCo
     # If appropriate, ...
     # Save the updated orbit to the db
     # Save the status to the db
+    
+    
     
 def call_orbfit_via_commandline_update_wrapper(unpacked_provisional_designation):
     """
@@ -166,30 +201,47 @@ def call_orbfit_via_commandline_update_wrapper(unpacked_provisional_designation)
     return result_dict
     
 
-def assess_quality_dict(quality_dict):
-    """ At present this is just saying Bad/Red(-1), SlightProblem/Amber(0), Good/Green(1) """
+def assess_quality_dict(quality_dict , boolean_dict):
+    """ At present this is just setting one of the following booleans in the boolean_dict ...
+        'HAS_BAD_QUALITY_DICT'            : False,
+        'HAS_INTERMEDIATE_QUALITY_DICT'   : False,
+        'HAS_GOOD_QUALITY_DICT'           : False,
+    """
     
     # Things to loop through
     expected_topline_keys = ["mid_epoch","std_epoch"]
     problems_A  = ["no orbit"]
     problems_B  = ["no CAR covariance", "no COM covariance"]
-    
-    # Default result dict
-    result_dict = {'status':1}
-    
-    # Go through problems: most severe first
-    for k in expected_topline_keys:
-        message_string = quality_dict[k]
-        for problem in problems_A:
+
+    #Default is "good"
+    boolean_dict['HAS_BAD_QUALITY_DICT']            = False
+    boolean_dict['HAS_INTERMEDIATE_QUALITY_DICT']   = False
+    boolean_dict['HAS_GOOD_QUALITY_DICT']           = True
+
+        
+    # Severe problems
+    for problem in problems_A:
+        # Loop through the different epoch-keys, examining the message-string for each
+        for k in expected_topline_keys:
+            message_string = quality_dict[k]
             if problem in message_string:
-                    result_dict['status'] = -1
-                    break
-        for problem in problems_B:
+                boolean_dict['HAS_BAD_QUALITY_DICT']            = True
+                boolean_dict['HAS_INTERMEDIATE_QUALITY_DICT']   = False
+                boolean_dict['HAS_GOOD_QUALITY_DICT']           = False
+                return
+                
+    # Intermediate problems
+    for problem in problems_B:
+        # Loop through the different epoch-keys, examining the message-string for each
+        for k in expected_topline_keys:
+            message_string = quality_dict[k]
             if problem in message_string:
-                    result_dict['status'] = 0
-                    break
+                boolean_dict['HAS_BAD_QUALITY_DICT']            = False
+                boolean_dict['HAS_INTERMEDIATE_QUALITY_DICT']   = True
+                boolean_dict['HAS_GOOD_QUALITY_DICT']           = False
+                return
     # return
-    return result_dict
+    return
     
     
 if __name__ == '__main__':
