a163c60a59f7ae684b4ef6ad8c9c8be299eda710
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-02-26 14:00:55 -0500

    developing logic

--- orbit_checker.py
+++ orbit_checker.py
@@ -25,11 +25,10 @@ import glob
 # --------- Local imports -----------
 sys.path.insert(0,'/share/apps/identifications_pipeline/dbchecks/')
 import query_ids
-
 import db_query_orbits as query_orbs
 
 sys.path.insert(0,'/sa/orbit_pipeline/')
-
+import update_wrapper
 
 
 
@@ -56,26 +55,31 @@ status_dict = {
 boolean_dict = {
 
 # Overall designation status
-'IS_PRIMARY_UNPACKED_DESIGNATION' : False,
+'IS_PRIMARY_UNPACKED_DESIGNATION'   : False,
 
 # Whether an orbit exists anywhere
-'IS_IN_ORBFIT_RESULTS'            : False,
-'IS_IN_COMET_RESULTS'             : False,
-'IS_IN_SATELLITE_RESULTS'         : False,
-'IS_IN_NO_RESULTS'                : False,
+'IS_IN_ORBFIT_RESULTS'              : False,
+'IS_IN_COMET_RESULTS'               : False,
+'IS_IN_SATELLITE_RESULTS'           : False,
+'IS_IN_NO_RESULTS'                  : False,
 
 # If has orbfit results in table, what is overall summary of the "quality-json"
-'HAS_BAD_QUALITY_DICT'            : False,
-'HAS_INTERMEDIATE_QUALITY_DICT'   : False,
-'HAS_GOOD_QUALITY_DICT'           : False,
+'HAS_BAD_QUALITY_DICT'              : False,
+'HAS_INTERMEDIATE_QUALITY_DICT'     : False,
+'HAS_GOOD_QUALITY_DICT'             : False,
 
-# Are there any problems when we try to get observations to do fit
-'HAS_NO_OBSERVATIONS'             : False,
-'HAS_FEW_OBSERVATIONS'            : False,
-'HAS_MANY_OBSERVATIONS'           : False,
+# Were there problems running orbfit (I.E. complete failures in start-up / crashes / etc )
+# NB: Should subsequently flag-up problems with input obs/orbits/other
+'SUCCESSFUL_ORBFIT_EXECUTION'       : False,
+
+# Given successful execution, could an orbit of any kind be generated ( even if there are some outliers, or the fit is "weak" )
+'SUCCESSFUL_ORBIT_GENERATION'       : False,
 
 # Are there any obvious outlier tracklets when we run fit (bad tracklet dict)
-'HAS_ERRONEOUS_TRACKLETS'         : False,
+'HAS_BAD_TRACKLETS'                 : False,
+
+# Are there any obvious outlier tracklets when we run fit (bad tracklet dict)
+'HAS_WEAK_ORBIT_FIT'                : False,
 
 }
 
@@ -152,7 +156,8 @@ def check_single_designation( unpacked_provisional_designation , dbConnIDs, dbCo
             print(k, boolean_dict[k])
 
     # Attempt to fit the orbit using the "orbit_pipeline_wrapper"
-    result_dict = call_orbfit_via_commandline_update_wrapper(unpacked_provisional_designation)
+    #result_dict = call_orbfit_via_commandline_update_wrapper(unpacked_provisional_designation)
+    result_dict = direct_call_orbfit_update_wrapper(unpacked_provisional_designation)
     assess_result_dict(result_dict , boolean_dict)
     
     # Evaluate the result from the orbit_pipeline_wrapper & assign a status
@@ -204,6 +209,31 @@ def call_orbfit_via_commandline_update_wrapper(unpacked_provisional_designation)
         result_dict = json.load(rj)
     return result_dict
     
+    
+def direct_call_orbfit_update_wrapper(unpacked_provisional_designation):
+    """
+    # Attempt to fit the orbit using the "orbit_pipeline_wrapper"
+    """
+    arg_dict = {
+        'obs80_filepath'        :       None,
+        'psv_filepath'          :       None,
+        'xml_filepath'          :       None,
+        'object_list'           :       [unpacked_provisional_designation],
+        'els_ext'               :       None,
+        'primary_desig_file'    :       None,
+        'usefindn'              :       True,
+        'queue_name'            :       'mba/mopp',
+        'elements_dir'          :       'neofitels/',
+        'obs_dir'               :       'res/',
+        'mpecfiles_dir'         :       'mpecfiles/',
+        'cov_dir'               :       'cov/',
+        'res_analysis_dir'      :       'badtrkfiles/',
+        'findn_dir'             :       'findnfiles/',
+        'proc_subdir'           :       'check_obj',
+        'std_epoch'             :       '59200'
+    }
+    result_dict = update_wrapper.update_wrapper( arg_dict )
+    
 
 def assess_quality_dict(quality_dict , boolean_dict):
     """ At present this is just setting one of the following booleans in the boolean_dict ...
@@ -247,6 +277,7 @@ def assess_quality_dict(quality_dict , boolean_dict):
     # return
     return
     
+    
 def assess_result_dict(result_dict , boolean_dict):
     """ Assess the results retturned by orbfit
     
@@ -259,33 +290,49 @@ def assess_result_dict(result_dict , boolean_dict):
         
     """
     
+    print(result_dict)
+    '''
     # There can be problems w.r.t. the input generation ...
     # *** Need to discuss with MPan how to interpret ***
     # The known failure is K15XM9X == 2015 XX229
     #
     # If certain keys are absent, => didn't run => look for set-up failure ...
     # Expect keys like 'K15XM9X' , 'batch', 'obs_summary', 'time', 'top_level'
-    ORBFIT_SUCCESS = False if 'failedfits' not in result_dict else True
+    SUCCESSFUL_ORBFIT_EXECUTION = False if 'failedfits' not in result_dict else True
         
     # Perhaps it ran but we get an explicit indicate of failure
-    if ORBFIT_SUCCESS:
-        ORBFIT_SUCCESS = False if result_dict['failedfits'] else True # If we see something in failedfits, then this is a failure
+    if SUCCESSFUL_ORBFIT_EXECUTION:
+        SUCCESSFUL_ORBFIT_EXECUTION = False if result_dict['failedfits'] else True # If we see something in failedfits, then this is a failure
 
-    print('ORBFIT_SUCCESS = ',ORBFIT_SUCCESS )
+    print('SUCCESSFUL_ORBFIT_EXECUTION = ',SUCCESSFUL_ORBFIT_EXECUTION )
     
-    # Assuming we have some sort of success, then look for bad-tracklets
-    if ORBFIT_SUCCESS and result_dict['badtrkdict']:
-        BAD_TRACKLETS_EXIST = True
-        print( "BAD_TRACKLETS_EXIST : " , result_dict['badtrkdict'] )
         
-    # Let's look for the quality dict ...
+    # For whatever reason we do not have a quality-dict (must be in another piece of code)
+    # But we do have a "fit_status"...
+    """
+    'fit_status' : string describing outcome of fit
+    'no problems' : good fit
+    'N bad tracklet(s)' : good fit but some obs are far off (>10 arcsec) and should be dissociated
+    'weak fit, no chisq' : there is an orbit, but Orbfit thought it was too shaky to trust a chisq calculation
+    'no fit' : Orbfit couldn't get a fit
+    """
     if ORBFIT_SUCCESS:
         for k,v in result_dict.items():
             if isinstance(v, dict) and 'fit_status' in v:
-                print(k)
-                for key,val in v.items():
-                    print('\t',key)
-                    if key == 'fit_status':
-                        print('\t\t',val)
+                fit_status = v['fit_status']
+                print('fit_status', fit_status)
+                
+                SUCCESSFUL_ORBIT_GENERATION = False if fit_status == 'no fit' else True
+                HAS_BAD_TRACKLETS           = True  if fit_status == 'N bad tracklet(s)' else False
+                HAS_WEAK_ORBIT_FIT          = True  if fit_status == 'weak fit, no chisq' else False
+
+
+        
+                if HAS_BAD_TRACKLETS:
+                    print( "BAD_TRACKLETS_EXIST : " , result_dict['badtrkdict'] )
+    '''
+                        
+
+                       
 if __name__ == '__main__':
     check_multiple_designations(method = 'ALL' , size=1 )
