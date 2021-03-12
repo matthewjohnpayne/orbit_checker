5ace440e57e0ac69528c6fb115db4d95f10fc2e1
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-03-12 11:39:42 -0500

    working towards db-write functionality

--- orbit_checker.py
+++ orbit_checker.py
@@ -349,4 +349,4 @@ def assess_result_dict(unpacked_provisional_designation , result_dict):
 
                        
 if __name__ == '__main__':
-    check_multiple_designations(method = 'RANDOM' , size=200 )
+    check_multiple_designations(method = 'RANDOM' , size=2000 )
