6ece4c76ca1f96fe7c4abd92a95bd2914d46cfd9
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-03-12 14:29:47 -0500

    working towards db-write functionality

--- orbit_checker.py
+++ orbit_checker.py
@@ -105,8 +105,8 @@ def check_multiple_designations( method = None , size=0 ):
 
     # Get a list of primary designations from the current_identifications table in the database
     if method in ['ALL' ,'RANDOM']:
-        primary_designations_array =  np.array(['2015 BC470', '2016 QW66'])
-        '''
+        #primary_designations_array =  np.array(['2015 BC470', '2016 QW66'])
+        
         print("\n... Searching db for all primary designations ... ")
         primary_designations_list_of_dicts = dbConnIDs.get_unpacked_primary_desigs_list()
         
@@ -114,7 +114,7 @@ def check_multiple_designations( method = None , size=0 ):
         # filter-out "/" which we see in satellites (currently causes update-wrapper to crash)
         # filter-out "A" at the start of the designation, as this currently causes packed_to_unpacked_desig to crash
         primary_designations_array         = np.array( [ d['unpacked_primary_provisional_designation'] for d in primary_designations_list_of_dicts if "/" not in d['unpacked_primary_provisional_designation']  and "A" != d['unpacked_primary_provisional_designation'][0] ] )
-        '''
+        
         
     # Choose a random subset
     if method == 'RANDOM':
@@ -350,4 +350,4 @@ def assess_result_dict(unpacked_provisional_designation , result_dict):
 
                        
 if __name__ == '__main__':
-    check_multiple_designations(method = 'RANDOM' , size=2 )
+    check_multiple_designations(method = 'RANDOM' , size=2000 )
