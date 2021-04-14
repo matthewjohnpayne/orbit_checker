bdf3223d25ac024e9ede4c2dfbed20a82d1c024a
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-04-14 13:50:39 -0400

    various

--- orbit_checker.py
+++ orbit_checker.py
@@ -592,10 +592,10 @@ def assess_result_dict(designation_dict , result_dict):
 def save_results_to_database(designation_dict, assessment_dict, result_dict , destination = 'asteroid'):
     ''' Save results to table(s) ...'''
     print('save_results_to_database: destination = ', destination)
-    assert False
     
     if assessment_dict['SUCCESSFUL_ORBFIT_EXECUTION']:
         if designation_dict['packed'] in result_dict:
+            sys.exit('exiting instead of writing ...')
             # Call the code to insert the results into the database
             to_db.main( [packed] , filedictlist=[result_dict[packed]] )
 
